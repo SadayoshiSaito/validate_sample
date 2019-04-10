394b85422d227bac65c9b39b1e7c1d4eec888ee8
sadapi_100 <sadapi_100@yahoo.co.jp>
2019-04-11 08:24:15 +0900

    0411

---
+++ app/Http/Controllers/ValidateTestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ValidateTestController extends Controller
+{
+    //
+    public function create()
+    {
+        return view('create');
+    }
+}
